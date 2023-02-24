9daf9193eef3a38217165560a6b973b1541d9072
Gev <tradingtrader@mail.ru>
2023-02-24 23:17:09 +0400

    added object in 3 languages

---
+++ api/app/Http/Controllers/HomeController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function getEng () {
+
+        $obj = [
+            "header_rent" => "For Rent",
+            "header_sale"=> "For Sale",
+            "header_services"=> "Our Services",
+            "header_contact"=> "Contact Us"
+        ];
+        return response()->json($obj);
+    }
+
+    public function getRu () {
+
+        $obj = [
+            "header_rent" => "Аренда",
+            "header_sale"=> "Продается",
+            "header_services"=> "Наши услуги",
+            "header_contact"=> "Свяжитесь с нами"
+        ];
+        return response()->json($obj);
+    }
+
+    public function getArm () {
+
+        $obj = [
+            "header_rent" => "Տրվում է վարձով",
+            "header_sale"=> "Վաճառվում է",
+            "header_services"=> "Մեր ծառայությունները",
+            "header_contact"=> "Կապ մեզ հետ"
+        ];
+        
+        return response()->json($obj);
+    }
+}
--- api/routes/api.php
+++ api/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::group(['middleware' => 'api'], function ($router) {
+    Route::get('/eng', [HomeController::class, 'getEng']);
+    Route::get('/ru', [HomeController::class, 'getRu']);
+    Route::get('/arm', [HomeController::class, 'getArm']);
 });
