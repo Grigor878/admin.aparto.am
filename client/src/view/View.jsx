e041db1efbfb7009a527dea8a614e30873e9cb03
Grigor878 <grigsgog@gmail.com>
2023-02-24 23:07:10 +0400

    errors fixed

--- client/src/view/View.jsx
+++ client/src/view/View.jsx
@@ -2,22 +2,22 @@ import React, { useEffect, Suspense } from "react"
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Header from "../components/Header/Header"
+import Header from "../components/header/Header"
 import AutoScroll from "../helpers/autoScroll"
 import HelmetAsync from "../components/helmetAsync/HelmetAsync"
 import Loading from "../components/loading/Loading"
-import Footer from "../components/Footer/Footer"
-const Admin = React.lazy(() => import('../admin/Admin'))
+import Footer from "../components/footer/Footer"
+const Home = React.lazy(() => import('../pages/home/Home'))
 const Rent = React.lazy(() => import('../pages/rent/Rent'))
 const SubRent = React.lazy(() => import('../pages/rent/subRent/SubRent'))
 const Sale = React.lazy(() => import('../pages/sale/Sale'))
 const SubSale = React.lazy(() => import('../pages/sale/subSale/SubSale'))
 const Services = React.lazy(() => import('../pages/services/Services'))
 const Contact = React.lazy(() => import('../pages/contact/Contact'))
+const Admin = React.lazy(() => import('../admin/Admin'))
 const NotFound = React.lazy(() => import('../pages/404/NotFound'))
 
 const View = () => {
-
     useEffect(() => {
         AOS.init();
     }, [])
@@ -29,7 +29,7 @@ const View = () => {
             <HelmetAsync />
             <Suspense fallback={<Loading />}>
                 <Routes>
-                    <Route exact path="/" />
+                    <Route exact path="/" component={<Home />} />
                     <Route exact path="/for-rent" component={<Rent />} />
                     <Route path="/for-rent/:id" component={<SubRent />} />
                     <Route exact path="/for-sale" component={<Sale />} />
