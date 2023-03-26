14319a4ba2392e09ff288b4bbf16513728d0b425
Grigor878 <grigsgog@gmail.com>
2023-03-26 18:04:23 +0400

    LogIn,LogOut,View

--- client/src/admin/pages/login/Login.jsx
+++ client/src/admin/pages/login/Login.jsx
@@ -1,111 +1,41 @@
 import React, { useState } from 'react'
-import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import axios from "axios"
 import { API_BASE_URL } from '../../../config'
 
 
 const Login = () => {
+    const navigate = useNavigate()
 
-    const [values, setValues] = useState({
-        email: '',
-        password: '',
-    })
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
-    const getSigninValue = (e) => {
-        const { name, value } = e.target
-        setValues({
-            ...values,
-            [name]: value
+    const handleApi = () => {
+        // console.log({ email, password })
+        axios.post(API_BASE_URL + "/api/signin", {
+            email: email,
+            password: password
         })
-    }
-
-
-    const getData = () => {
-        let data = {}
-        data = values
-
-        axios.post(API_BASE_URL + "/api/signin", data)
-            .then(
-                response => {
-                    const token = response.data.access_token
-                    localStorage.setItem('auth', true)
-                    localStorage.setItem('authUser', token)
-                    console.log(response.data, 88)
-
-                    window.location.reload(false);
-                })
-            .catch(error => {
-                console.log(`Error - ${error.message}`)
+            .then(res => {
+                // console.log(res.data, 88)
+                localStorage.setItem('auth', true)
+                localStorage.setItem('authUser', res.data.access_token)
+                navigate('/dashboard')
+                window.location.reload(false)
+            })
+            .catch(err => {
+                console.log(err.message)
             })
     }
-
-    localStorage.getItem("auth") ? <Navigate to='/dashboard' /> : <></>
 
     return (
         <div>
             <h1>Login page</h1>
-            <div>
-                <div >
-                    <label htmlFor="form3Example3">Email address</label><br />
-                    <input style={{ border: "1px solid black" }} type="email" name="email" value={values.email} onChange={getSigninValue} className="form-control form-control-lg" />
-                </div>
-                <div >
-                    <label htmlFor="form3Example4">Password</label> <br />
-                    <input style={{ border: "1px solid black" }} type="password" name="password" value={values.password} onChange={getSigninValue} className="form-control form-control-lg" />
-                </div>
-                <div >
-                    <button onClick={getData} type="button" style={{ paddingLeft: '2.5rem', paddingRight: '2.5rem' }}>Login</button>
-                </div>
-            </div>
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+            <button onClick={handleApi}>Log-In</button>
         </div>
     )
 }
 
 export default Login
-
-// import { useState } from "react"
-// import { useNavigate } from "react-router-dom";
-
-// const Login = () => {
-
-//     const navigate = useNavigate()
-
-//     const [username, setusername] = useState("")
-//     const [password, setpassword] = useState("")
-
-//     const [authenticated, setauthenticated] = useState(
-//         localStorage.getItem(localStorage.getItem("auth") || false)
-//     );
-
-//     const users = [{ username: "grigsgog@gmail.com", password: "123456" }]
-
-//     const handleSubmit = (e) => {
-//         e.preventDefault()
-//         const account = users.find((user) => user.username === username)
-//         if (account && account.password === password) {
-//             localStorage.setItem("auth", true)
-//             navigate("/dashboard")
-//         }
-//     }
-
-//     return (
-//         <div>
-//             <form onSubmit={handleSubmit}>
-//                 <input
-//                     type="text"
-//                     name="Username"
-//                     value={username}
-//                     onChange={(e) => setusername(e.target.value)}
-//                 />
-//                 <input
-//                     type="password"
-//                     name="Password"
-//                     onChange={(e) => setpassword(e.target.value)}
-//                 />
-//                 <input type="submit" value="Submit" />
-//             </form>
-//         </div>
-//     )
-// }
-
-// export default Login;
