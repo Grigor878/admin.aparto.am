d56fa9965fc834f1c8c3b1bd22d924dc63363e43
Grigor878 <grigsgog@gmail.com>
2023-06-11 02:20:45 +0400

    undefined Properties fixed,properties search fixed

--- client/src/admin/components/topPart/TopPart.jsx
+++ client/src/admin/components/topPart/TopPart.jsx
@@ -16,7 +16,7 @@ const TopPart = ({ data, type }) => {
             <h3>
                 {/* {data?.length === 1 ? data?.length + ' ' + capitalize(newPath.slice(0, -1)) : data?.length === 0 ? 'No Data' : data?.length + ' ' + capitalize(newPath)} */}
                 {newPath === "users" ? data?.length + " Օգտատեր" : null}
-                {newPath === "properties" ? data?.length + " Գույք" : null}
+                {newPath === "properties" && data?.length !== undefined ? data?.length + " Գույք" : null}
             </h3>
 
             {(role === "admin" && type === "users") || type === "properties"
