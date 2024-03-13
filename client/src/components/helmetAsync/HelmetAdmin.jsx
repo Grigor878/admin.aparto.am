f99082f82541f5399ca77b1bde2a055ff3ef2d9a
Grigor Safaryan <grigor.safaryan@ekeng.am>
2024-03-13 16:21:11 +0400

    helmet provider improved

--- client/src/components/helmetAsync/HelmetAdmin.jsx
+++ client/src/components/helmetAsync/HelmetAdmin.jsx
@@ -1,24 +1,26 @@
-import { Helmet } from "react-helmet-async"
-import { useLocation } from "react-router-dom"
+import { HelmetProvider, Helmet } from "react-helmet-async";
+import { useLocation } from "react-router-dom";
 
 const HelmetAdmin = () => {
-    const { pathname } = useLocation();
+  const { pathname } = useLocation();
 
-    function capitalize() {
-        return pathname.charAt(1).toUpperCase() + pathname.slice(2);
-    }
+  function capitalize() {
+    return pathname.charAt(1).toUpperCase() + pathname.slice(2);
+  }
 
-    let newPath = capitalize(pathname.substring(1));
+  let newPath = capitalize(pathname.substring(1));
 
-    if (newPath !== "") {
-        newPath = "| " + newPath;
-    }
+  if (newPath !== "") {
+    newPath = "| " + newPath;
+  }
 
-    return (
-            <Helmet>
-                <title>Aparto {newPath}</title>
-            </Helmet>
-    );
-}
+  return (
+    <HelmetProvider>
+      <Helmet>
+        <title>Aparto {newPath}</title>
+      </Helmet>
+    </HelmetProvider>
+  );
+};
 
-export default HelmetAdmin
+export default HelmetAdmin;
