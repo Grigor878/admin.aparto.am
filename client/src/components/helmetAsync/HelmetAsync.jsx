d87787e7aa5d96124edf97475b9c20e316d34a7f
Grigor Safaryan <grigor.safaryan@ekeng.am>
2024-03-13 15:13:39 +0400

    index and helmet changes'

--- client/src/components/helmetAsync/HelmetAsync.jsx
+++ client/src/components/helmetAsync/HelmetAsync.jsx
@@ -21,9 +21,10 @@ const HelmetAsync = ({ description, image }) => {
   const title = `Aparto ${newPath}`;
   const url = "https://aparto.am";
   const name = "Aparto";
-  // const logo = "https://aparto.am/static/media/logo.c81fd539113588de5f95.png";
-  const logo = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB8AAAAgCAMAAADdXFNzAAAAgVBMVEVHcEwwZc8gWMk/d+c6bdI+b9BBeOclWcI6cd82a9c2a9UxZs9Ec9E+deM+duY/asANRbQrY9QwW7RAcdNWheR4n+sHPq1Nb7JAeeonX89Bd+QgWMkbU8NBduAUTLxAc9lBdd1AcdMvaNk2b+ENRLQ7dOY9acA1Z8pojdZYe8F2nOkXrFO5AAAAGHRSTlMA0Le2SA7sGCpqh76tkdq+6uyL7Va9xNrIabguAAABHklEQVQoka3S13qDMAwGUJbDKKNZLTOMAjG8/wP2lzEzkJvWV4aD9ElCivK/x3qvYdi8++IE94/ZjuHN7dAzeNj8HLEj3dlnlks/SOBO7u6xVRZwnbwyd1zN4b7ik6uvbJTklmKRV8aLX8l1XHTy85btEh4z3ExwFdkbJ848cfXIozU7Ilw+EEerITERPua04UnCFu6Sn6bHM7k2syXC554M8nReBJV4+d9V8mlIRj6MZpEQnD7GhFdifdWPRv4pyy3AMVu5An7UQ0NFBsZobvd7EAQfOJqmCa+JPWgYogre933bPp8YDmUnv8BPxMjkcs6li+rANVWgY2doNJeu41x4MobXX3hvYulFb99d1w6eyvB62KNxm5i5Ocrfzy8r5yS4d3fNSgAAAABJRU5ErkJggg==";
+  const logo = "https://aparto.am/static/media/logo.c81fd539113588de5f95.png";
   const _image = image || logo;
+  const _imageWidth = 600;
+  const _imageHeight = 600;
 
   return (
     <Helmet>
@@ -35,9 +36,9 @@ const HelmetAsync = ({ description, image }) => {
       {/* Open Graph / Facebook  */}
       <meta property="og:image" content={_image} />
       <meta property="og:image:secure_url" content={_image} />
-      <meta property="og:image:width" content="600" />
-      <meta property="og:image:height" content="600" />
-      <meta property="og:image:alt" content="Image" />
+      <meta property="og:image:width" content={_imageWidth} />
+      <meta property="og:image:height" content={_imageHeight} />
+      <meta property="og:image:alt" content="image" />
       <meta property="og:type" content="website" />
       <meta property="og:url" content={url + pathname} />
       <meta property="og:title" content={title} />
