aed59251a4008385ecb9a8c7597b7a754a79b39b
Grigor Safaryan <grigor.safaryan@ekeng.am>
2024-03-13 15:47:23 +0400

    fb:app_id created and integrated

--- client/src/components/helmetAsync/HelmetAsync.jsx
+++ client/src/components/helmetAsync/HelmetAsync.jsx
@@ -3,7 +3,7 @@ import { useTranslation } from "react-i18next";
 import { useLocation } from "react-router-dom";
 // import logo from "../../assets/imgs/favicon.ico";
 
-const HelmetAsync = ({ description, image }) => {
+const HelmetAsync = ({ description, keywords, image }) => {
   const { t } = useTranslation();
 
   const { pathname } = useLocation();
@@ -32,17 +32,19 @@ const HelmetAsync = ({ description, image }) => {
 
       <meta name="title" content={title} />
       <meta name="description" content={t(description)} />
+      <meta name="keywords" content={keywords} />
 
       {/* Open Graph / Facebook  */}
+      <meta property="og:url" content={url + pathname} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={t(description)} />
       <meta property="og:image" content={_image} />
       <meta property="og:image:secure_url" content={_image} />
       <meta property="og:image:width" content={_imageWidth} />
       <meta property="og:image:height" content={_imageHeight} />
       <meta property="og:image:alt" content="image" />
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content={url + pathname} />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={t(description)} />
+      <meta property="fb:app_id" content="946682250427136" />
 
       {/* Twitter */}
       <meta name="twitter:creator" content={name} />
