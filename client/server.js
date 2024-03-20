f8fe89d0b4717787d935dc72212e5216282a4785
Grigor Safaryan <grigor.safaryan@ekeng.am>
2024-03-20 16:16:38 +0400

    server.js changes

--- client/server.js
+++ client/server.js
@@ -1,48 +1,3 @@
-// const express = require('express')
-// const path = require('path')
-// const fs = require('fs')
-
-// const PORT = 5000
-
-// const app = express()
-
-// app.use(express.static(path.resolve(__dirname, "./build")));
-
-// app.get("/", (req, res) => {
-//     const filePath = path.resolve(__dirname, "./build", "index.html")
-//     fs.readFile(filePath, "utf8", (err, data) => {
-//         if (err) {
-//             return console.log(err)
-//         }
-
-//         data = data.replace(/__TITLE__/g, "Aparto")
-//             .replace(/__DESCRIPTION__/g, "Discover Property to Buy or Rent")
-//             .replace(/__IMAGE__/g, "https://aparto.am/static/media/logo.c81fd539113588de5f95.png")
-
-//         res.send(data)
-//     })
-// })
-
-// app.get("/result", (req, res) => {
-//     const filePath = path.resolve(__dirname, "./build", "index.html")
-//     fs.readFile(filePath, "utf8", (err, data) => {
-//         if (err) {
-//             return console.log(err)
-//         }
-
-//         data = data.replace(/__TITLE__/g, "Aparto | Result")
-//             .replace(/__DESCRIPTION__/g, "Result")
-//             .replace(/__IMAGE__/g, "")
-//             .replace(/__URL__/g, "https://aparto.am/result")
-
-//         res.send(data)
-//     })
-// })
-
-// app.listen(PORT, () => {
-//     console.log("Server is listening on port " + PORT);
-// })
-
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
@@ -50,12 +5,9 @@ const fs = require('fs');
 const PORT = 3001;
 const app = express();
 
-// Serve static files from the build folder
 app.use(express.static(path.resolve(__dirname, "./build")));
 
-// Handle dynamic content for "/"
 app.get("/", (req, res) => {
-    // Read the index.html file
     const filePath = path.resolve(__dirname, "./build", "index.html");
     fs.readFile(filePath, "utf8", (err, data) => {
         if (err) {
@@ -63,7 +15,6 @@ app.get("/", (req, res) => {
             return res.status(500).send('Internal Server Error');
         }
 
-        // Replace placeholders with actual values
         data = data.replace(/__TITLE__/g, "Aparto")
                    .replace(/__DESCRIPTION__/g, "Discover Property to Buy or Rent")
                    .replace(/__IMAGE__/g, "https://aparto.am/static/media/logo.c81fd539113588de5f95.png");
@@ -72,9 +23,7 @@ app.get("/", (req, res) => {
     });
 });
 
-// Handle dynamic content for "/result"
 app.get("/result", (req, res) => {
-    // Read the index.html file
     const filePath = path.resolve(__dirname, "./build", "index.html");
     fs.readFile(filePath, "utf8", (err, data) => {
         if (err) {
@@ -82,7 +31,6 @@ app.get("/result", (req, res) => {
             return res.status(500).send('Internal Server Error');
         }
 
-        // Replace placeholders with actual values
         data = data.replace(/__TITLE__/g, "Aparto | Result")
                    .replace(/__DESCRIPTION__/g, "Result")
                    .replace(/__IMAGE__/g, "")
@@ -92,7 +40,6 @@ app.get("/result", (req, res) => {
     });
 });
 
-// Start the server
 app.listen(PORT, () => {
     console.log("Server is listening on port " + PORT);
 });
