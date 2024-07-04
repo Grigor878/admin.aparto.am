5f259dff95c141497cecf721e9b7be5c65992d8a
Grigor Safaryan <grigor.safaryan@ekeng.am>
2024-07-04 09:48:13 +0400

    search history mobile and desctop new versions

--- client/src/pages/home/components/searches/Searches.jsx
+++ client/src/pages/home/components/searches/Searches.jsx
@@ -1,99 +1,135 @@
-import React from 'react'
-import { useTranslation } from 'react-i18next'
-import { search } from '../../../../assets/svgs/svgs'
-import { useDispatch, useSelector } from 'react-redux'
-import { usdFormater } from '../../../../helpers/formatters'
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { search } from "../../../../assets/svgs/svgs";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
 import cookies from "js-cookie";
-import './Searches.scss'
-import { useNavigate } from 'react-router-dom'
-import { clearSidertData, postSearchData, setPage } from '../../../../store/slices/viewSlice'
-import { addPrice, addPropertyType, addRooms, addTransactionType } from '../../../../store/slices/homeSlice'
+import { usdFormater } from "../../../../helpers/formatters";
+import {
+  clearSidertData,
+  postSearchData,
+  setPage,
+} from "../../../../store/slices/viewSlice";
+import {
+  addPrice,
+  addPropertyType,
+  addRooms,
+  addTransactionType,
+} from "../../../../store/slices/homeSlice";
+import "./Searches.scss";
 
 const Searches = () => {
-    const { t } = useTranslation()
+  const { t } = useTranslation();
 
-    const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-    const { language } = useSelector((state => state.home))
-    const { paginatePage, perPage } = useSelector((state => state.view))
+  const { language } = useSelector((state) => state.home);
+  const { paginatePage, perPage } = useSelector((state) => state.view);
+  const mobile = useMediaQuery({ maxWidth: 768 });
 
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    // const searchHistory = JSON.parse(cookies.get("searchHistory") || "[]");
-    const searchHistory = JSON.parse(cookies.get("searchHistory") || "[]")?.slice(-6);
+  // const searchHistory = JSON.parse(cookies.get("searchHistory") || "[]");
+  const searchHistory = mobile
+    ? JSON.parse(cookies.get("searchHistory") || "[]")
+    : JSON.parse(cookies.get("searchHistory") || "[]")?.slice(-6);
 
-    const handleSearch = (type, community, propertyType, rooms, price) => {
-        const searchData = [
-            { type },
-            { community },
-            { propertyType },
-            { rooms },
-            { price },
-            { page: paginatePage },
-            { perPage: perPage }
-        ]
-        dispatch(addTransactionType(type))
-        dispatch(addPropertyType(propertyType))
-        dispatch(addRooms(rooms))
-        dispatch(addPrice(price))
-        dispatch(clearSidertData())
-        dispatch(setPage("home"))
-        dispatch(postSearchData({ searchData, language }))
-            .then(() => {
-                navigate("/result")
-            })
-    }
+  const handleSearch = (type, community, propertyType, rooms, price) => {
+    const searchData = [
+      { type },
+      { community },
+      { propertyType },
+      { rooms },
+      { price },
+      { page: paginatePage },
+      { perPage: perPage },
+    ];
+    dispatch(addTransactionType(type));
+    dispatch(addPropertyType(propertyType));
+    dispatch(addRooms(rooms));
+    dispatch(addPrice(price));
+    dispatch(clearSidertData());
+    dispatch(setPage("home"));
+    dispatch(postSearchData({ searchData, language })).then(() => {
+      navigate("/result");
+    });
+  };
 
-    return (
-        searchHistory?.length > 0 &&
-        <div className='searches block'>
-            <h2 className='title'>{t("searches")}</h2>
-            <div className="searches__row">
-                {searchHistory?.map(({ id, type, community, propertyType, price, rooms }) => {
-                    return (
-                        <div
-                            key={id}
-                            className="searches__col"
-                            onClick={() => handleSearch(type, community, propertyType, rooms, price)}
-                        >
-                            {search.icon}
-                            <div className="searches__col-context">
-                                <p>
-                                    {propertyType.map(type => {
-                                        return type === "house" ? t("house")
-                                            : type === "privateHouse" ? t("private_house")
-                                                : type === "commercial" ? t("commercial")
-                                                    : null;
-                                    }).join(",")}
-                                    {"  "}
-                                    {type === "sale" ? t("sale").toLowerCase() : t("rent").toLowerCase()}
-                                </p>
-                                <span>
-                                    {community?.map((el, index) => (
-                                        index > 0 ? `,${el}` : el
-                                    ))}
+  return (
+    searchHistory?.length > 0 && (
+      <div className="searches block">
+        <h2 className="title">{t("searches")}</h2>
+        <div
+          className={
+            mobile ? "searches__row searches__row-mobile" : "searches__row"
+          }
+        >
+          {searchHistory?.map(
+            ({ id, type, community, propertyType, price, rooms }) => {
+              return (
+                <div
+                  key={id}
+                  className="searches__col"
+                  onClick={() =>
+                    handleSearch(type, community, propertyType, rooms, price)
+                  }
+                >
+                  {search.icon}
+                  <div className="searches__col-context">
+                    <p>
+                      {propertyType
+                        .map((type) => {
+                          return type === "house"
+                            ? t("house")
+                            : type === "privateHouse"
+                            ? t("private_house")
+                            : type === "commercial"
+                            ? t("commercial")
+                            : null;
+                        })
+                        .join(",")}
+                      {"  "}
+                      {type === "sale"
+                        ? t("sale").toLowerCase()
+                        : t("rent").toLowerCase()}
+                    </p>
+                    <span>
+                      {community?.map((el, index) =>
+                        index > 0 ? `,${el}` : el
+                      )}
 
-                                    {rooms?.length > 0 && <>
-                                        {community?.length > 0 && " | "}
-                                        {language === "en" ? t("number_of_bedrooms") : t("number_of_rooms")}
-                                        {" "}
-                                        {rooms?.map((el, index) => (
-                                            index > 0 ? `,${el}` : el
-                                        ))}
-                                    </>}
+                      {rooms?.length > 0 && (
+                        <>
+                          {community?.length > 0 && " | "}
+                          {language === "en"
+                            ? t("number_of_bedrooms")
+                            : t("number_of_rooms")}{" "}
+                          {rooms?.map((el, index) =>
+                            index > 0 ? `,${el}` : el
+                          )}
+                        </>
+                      )}
 
-                                    {(price && community?.length > 0) || (price && rooms?.length > 0) ? ` | ${usdFormater(price)}` : price ? ` ${usdFormater(price)}` : null}
-                                </span>
-                            </div>
-                        </div>
-                    )
-                })}
-            </div>
+                      {(price && community?.length > 0) ||
+                      (price && rooms?.length > 0)
+                        ? ` | ${usdFormater(price)}`
+                        : price
+                        ? ` ${usdFormater(price)}`
+                        : null}
+                    </span>
+                  </div>
+                </div>
+              );
+            }
+          )}
         </div>
+      </div>
     )
-}
+  );
+};
 
-export default Searches
+export default Searches;
 
 // .join('')
 
