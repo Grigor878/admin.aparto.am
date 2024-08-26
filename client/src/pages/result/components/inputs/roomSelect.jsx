f438681a0280a5a58d5488758efdc6af6c5c5b26
Grigor878 <grigsgog@gmail.com>
2024-08-26 23:37:44 +0400

    roomSelect ogic change

--- client/src/pages/result/components/inputs/roomSelect.jsx
+++ client/src/pages/result/components/inputs/roomSelect.jsx
@@ -1,32 +1,79 @@
+// import React from 'react';
+// import { bedroomsNum, roomsNum } from '../../../home/components/search/data';
+// import './styles.scss';
+
+// export const RoomSelect = ({ language, onChange, selectedRooms = [] }) => {
+//     const handleRoomClick = (value) => {
+//         const updatedSelectedRooms = selectedRooms?.includes(value)
+//             ? selectedRooms?.filter(room => room !== value)
+//             : [...selectedRooms, value];
+//         onChange(updatedSelectedRooms);
+//     };
+
+//     const roomData = language === "en" ? bedroomsNum : roomsNum;
+
+//     return (
+//         <div className='roomsSelector'>
+//             {roomData?.map((el) => (
+//                 <button
+//                     type="button"
+//                     key={el.value}
+//                     id={el.label}
+//                     value={el.value}
+//                     onClick={() => handleRoomClick(el.value)}
+//                     className='roomsSelector__btn'
+//                     style={{
+//                         backgroundColor: selectedRooms?.includes(el.value) ? "#cfd1da" : "#f3f4f8",
+//                     }}
+//                 >
+//                     {el.value}
+//                 </button>
+//             ))}
+//         </div>
+//     );
+// };
+
+
 import React from 'react';
+import { roomData } from '../../../home/components/search/data';
 import './styles.scss';
-import { bedroomsNum, roomsNum } from '../../../home/components/search/data';
 
 export const RoomSelect = ({ language, onChange, selectedRooms = [] }) => {
     const handleRoomClick = (value) => {
-        const updatedSelectedRooms = selectedRooms?.includes(value)
-            ? selectedRooms?.filter(room => room !== value)
+        const updatedSelectedRooms = selectedRooms.includes(value)
+            ? selectedRooms.filter((room) => room !== value)
             : [...selectedRooms, value];
         onChange(updatedSelectedRooms);
     };
 
-    const roomData = language === "en" ? bedroomsNum : roomsNum;
+    const adjustedRoomData = roomData?.map((el) => {
+        if (language === "en") {
+            const mappedLabel =
+                el.value === "studio"
+                    ? "1"
+                    : el.value === "6+"
+                        ? "7+"
+                        : (parseInt(el.value) + 1).toString();
+            return { ...el, label: mappedLabel };
+        }
+        return el;
+    });
 
     return (
-        <div className='roomsSelector'>
-            {roomData?.map((el) => (
+        <div className="roomsSelector">
+            {adjustedRoomData.map((el) => (
                 <button
                     type="button"
                     key={el.value}
                     id={el.label}
                     value={el.value}
                     onClick={() => handleRoomClick(el.value)}
-                    className='roomsSelector__btn'
+                    className="roomsSelector__btn"
                     style={{
-                        backgroundColor: selectedRooms?.includes(el.value) ? "#cfd1da" : "#f3f4f8",
+                        backgroundColor: selectedRooms.includes(el.value) ? "#cfd1da" : "#f3f4f8",
                     }}
                 >
-                    {el.value}
+                    {el.label}
                 </button>
             ))}
         </div>
